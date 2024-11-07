e42dd79339f1129a64936464d6e8449186985395
Eric <36084508+errmorra@users.noreply.github.com>
2024-11-07 14:28:01 -0600

    finalized V.1

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 let color = "black"
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(32);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "Now You Can Draw!";
+            }
+            else{
+                draw.innerHTML = "Please Click to Draw!"
+            }
+        }
+
+    })
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function(){
@@ -42,16 +57,22 @@ function getSize(){
 }
 
 function colorDiv(){
-    if(color == "random"){
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else{
+            this.style.backgroundColor = "black"
+        }
     }
-    else{
-        this.style.backgroundColor = "black"
-    }
-
 }
 
 function setColor(colorChoice){
     color = colorChoice;
 
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
