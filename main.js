00e7b2c6f2a95419ba3ad515c0194b9739295236
Eric <36084508+errmorra@users.noreply.github.com>
2024-11-07 07:35:08 -0600

    update 3

---
+++ main.js
@@ -0,0 +1,57 @@
+let color = "black"
+
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(32);
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
+})
+
+function createBoard(size){
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for(let i=0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend",div);
+    }
+
+}
+
+function getSize(){
+    let input = prompt("Choose the size of the board ?");
+    let message = document.querySelector("#message");
+    if(input==""){
+        message.innerHTML="Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100"
+    }
+    else{
+        message.innerHTML = "Now you can play!"
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(color == "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor = "black"
+    }
+
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+
+}
